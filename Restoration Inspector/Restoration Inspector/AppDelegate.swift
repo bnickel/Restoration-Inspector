39d7bd46b7891aa7bc162038f13d3e7af748326a
Brian Nickel <bnickel@stackexchange.com>
2015-03-12 14:53:10 -0700

    Open the chooser when no other windows are open.

--- Restoration Inspector/Restoration Inspector/AppDelegate.swift
+++ Restoration Inspector/Restoration Inspector/AppDelegate.swift
@@ -11,16 +11,34 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "windowWillClose:", name: NSWindowWillCloseNotification, object: nil)
+        
+    }
+    
+    func applicationWillTerminate(aNotification: NSNotification) {
+    }
+    
+    func applicationDidBecomeActive(notification: NSNotification) {
+        if NSApplication.sharedApplication().windows.count == 0 {
+            showSavedStatesWindow()
+        }
+    }
+    
     func applicationShouldOpenUntitledFile(sender: NSApplication) -> Bool {
         return false
     }
     
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+    func windowWillClose(aNotification: NSNotification) {
+        if NSApplication.sharedApplication().windows.count == 1 && (aNotification.object as? NSWindow)?.identifier != "All Saved States" {
+            showSavedStatesWindow()
+        }
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    
+    @IBOutlet weak var savedStatesMenuItem: NSMenuItem!
+    func showSavedStatesWindow() {
+        // As far as I can tell, this is the best way to create the controller and have the window retain the window controller.
+        NSApplication.sharedApplication().sendAction(savedStatesMenuItem.action, to: savedStatesMenuItem.target, from: savedStatesMenuItem)
     }
 }
 
